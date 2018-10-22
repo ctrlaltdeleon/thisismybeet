99a479d1b0b98dfdda35c642912ef6abfd74c557
AC De Leon <aadeleon@sdsu.edu>
2018-10-21 20:19:23 -0700

    [+] Working for mobile, changed color scheme, responsive design

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
 // User using keyboard
 function playByKeyboard(e) {
+  // Take the input with "e" and set the audio variable equal to audio[data-key=USER_INPUT]
+  // This will take care of the button music itself
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  // Take the input with "e" and set the key variable equal to div[data-key=USER_INPUT]
+  // This will take care of the button animation itself
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-  audio.currentTime = 0;
+  // Ensures when button is activated, the sound byte starts from the beginning
+  audio.currentTime = "0";
+  // Play the sound!
   audio.play();
+  // Animation for the button starts
   key.classList.add("playing");
 }
 
-// User using mouse
+// User using mouse or touch
 function playByClick(e) {
-  const keyCode = this.getAttribute("data-key"); // Get the attribute of the object clicked
+  // Get the attribute of the object clicked
+  const keyCode = this.getAttribute("data-key");
+  // Transform the input into readable input for the document.querySelector
+  // Take the input with "e" and set the audio variable equal to audio[data-key=USER_INPUT]
+  // This will take care of the button music itself
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  // Take the input with "e" and set the key variable equal to div[data-key=USER_INPUT]
+  // This will take care of the button animation itself
   const key = document.querySelector(`.key[data-key="${keyCode}"]`);
-  audio.currentTime = 0;
+  // Ensures when button is activated, the sound byte starts from the beginning
+  audio.currentTime = "0";
+  // Play the sound!
   audio.play();
+  // Animation for the button starts
   key.classList.add("playing");
 }
 
 // Smooth transition between keys
 function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
+  if (e.propertyName !== "transform") return undefined;
   e.target.classList.remove("playing");
 }
 
 // Listeners for the user
+// Find all elements in the document with a class "key"
 const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 keys.forEach(key => key.addEventListener("click", playByClick));
